132d1a40188862c45efaaf87cdbe836ebdf6f761
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-04-04 21:32:32 +0530

    fixes

--- src/bot/commands/index.ts
+++ src/bot/commands/index.ts
@@ -5,13 +5,15 @@ import { settings } from "./settings";
 import { executeStep } from "../executeStep";
 
 export function initiateBotCommands() {
-  teleBot.api.setMyCommands([
-    { command: "start", description: "Start the bot" },
-    {
-      command: "settings",
-      description: "Setup custom alerts by tweaking the settings",
-    },
-  ]);
+  teleBot.api
+    .setMyCommands([
+      { command: "start", description: "Start the bot" },
+      {
+        command: "settings",
+        description: "Setup custom alerts by tweaking the settings",
+      },
+    ])
+    .catch(() => null);
 
   teleBot.command("start", (ctx) => startBot(ctx));
   teleBot.command("settings", (ctx) => settings(ctx));
