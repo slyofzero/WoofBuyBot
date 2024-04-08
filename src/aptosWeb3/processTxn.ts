3def9ee163b52f33bb481be8b675a20a7b69ce71
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-04-08 22:21:51 +0530

    fixes

--- src/aptosWeb3/processTxn.ts
+++ src/aptosWeb3/processTxn.ts
@@ -1,99 +1,105 @@
 import { sendAlert } from "@/bot/sendAlert";
 import { AptosTransaction, ReceiversData } from "@/types";
-import { log } from "@/utils/handlers";
+import { errorHandler, log } from "@/utils/handlers";
 import { getTokenAddress, getTokenMetadata } from "@/utils/web3";
 import { prevTxns, setPrevTxn } from "@/vars/prevTxn";
 
 export async function processTxn(tx: AptosTransaction) {
-  const { events, changes, version } = tx;
-  if (!events?.length || !changes?.length) return false;
+  try {
+    const { events, changes, version } = tx;
+    if (!events?.length || !changes?.length) return false;
 
-  const receivers: ReceiversData = {};
+    const receivers: ReceiversData = {};
 
-  depositsLoop: for (const event of events) {
-    const receiver = event.guid.account_address;
-    const isTokenDeposit = event.type.endsWith("::coin::DepositEvent");
-    if (!isTokenDeposit) continue;
+    depositsLoop: for (const event of events) {
+      const receiver = event.guid.account_address;
+      const isTokenDeposit = event.type.endsWith("::coin::DepositEvent");
+      if (!isTokenDeposit) continue;
 
-    const { creation_number } = event.guid;
+      const { creation_number } = event.guid;
 
-    for (const change of changes) {
-      const changeData = change.data;
-      if (!changeData || change.address !== receiver) continue;
+      for (const change of changes) {
+        const changeData = change.data;
+        if (!changeData || change.address !== receiver) continue;
 
-      const changeCreationNum =
-        changeData.data.deposit_events?.guid.id.creation_num;
-      if (changeCreationNum !== creation_number) continue;
+        const changeCreationNum =
+          changeData.data.deposit_events?.guid.id.creation_num;
+        if (changeCreationNum !== creation_number) continue;
 
-      const token = getTokenAddress(changeData.type);
-      const metadata = await getTokenMetadata(token);
-      if (!metadata) continue depositsLoop;
-      const { decimals, symbol } = metadata;
+        const token = getTokenAddress(changeData.type);
+        const metadata = await getTokenMetadata(token);
+        if (!metadata) continue depositsLoop;
+        const { decimals, symbol } = metadata;
 
-      const amount = Number(event.data.amount) / 10 ** decimals;
+        const amount = Number(event.data.amount) / 10 ** decimals;
 
-      receivers[receiver] = {
-        token,
-        tokenReceived: symbol,
-        amountReceived: amount,
-        amountSent: 0,
-        tokenSent: "",
-        version,
-        receiver,
-      };
-    }
-  }
-
-  withdrawalsLoop: for (const event of events) {
-    const receiver = event.guid.account_address;
-    const isTokenDeposit = event.type.endsWith("::coin::WithdrawEvent");
-    const receiverData = receivers[receiver];
-    if (!isTokenDeposit || !receiverData) continue;
-
-    const { creation_number } = event.guid;
-
-    for (const change of changes) {
-      const changeData = change.data;
-      if (!changeData || change.address !== receiver) continue;
-
-      const changeCreationNum =
-        changeData.data.withdraw_events?.guid.id.creation_num;
-      if (changeCreationNum !== creation_number) continue;
-
-      const token = getTokenAddress(changeData.type);
-      const metadata = await getTokenMetadata(token);
-      if (!metadata) continue withdrawalsLoop;
-      const { decimals, symbol } = metadata;
-      const amount = Number(event.data.amount) / 10 ** decimals;
-
-      receivers[receiver] = {
-        ...receiverData,
-        tokenSent: symbol,
-        amountSent: amount,
-      };
-    }
-  }
-
-  for (const receiver in receivers) {
-    const receiverData = receivers[receiver];
-    const isMissingFields = Object.values(receiverData).some((value) => !value);
-    if (isMissingFields) {
-      delete receivers[receiver];
-      continue;
+        receivers[receiver] = {
+          token,
+          tokenReceived: symbol,
+          amountReceived: amount,
+          amountSent: 0,
+          tokenSent: "",
+          version,
+          receiver,
+        };
+      }
     }
 
-    const { tokenReceived, amountReceived, tokenSent, amountSent } =
-      receiverData;
-    const txn = `${version}_${amountReceived}_${tokenReceived}`;
+    withdrawalsLoop: for (const event of events) {
+      const receiver = event.guid.account_address;
+      const isTokenDeposit = event.type.endsWith("::coin::WithdrawEvent");
+      const receiverData = receivers[receiver];
+      if (!isTokenDeposit || !receiverData) continue;
 
-    if (!prevTxns.includes(txn)) {
-      sendAlert(receiverData);
-      log(
-        `${amountReceived} ${tokenReceived} for ${amountSent} ${tokenSent}, ${version}`
+      const { creation_number } = event.guid;
+
+      for (const change of changes) {
+        const changeData = change.data;
+        if (!changeData || change.address !== receiver) continue;
+
+        const changeCreationNum =
+          changeData.data.withdraw_events?.guid.id.creation_num;
+        if (changeCreationNum !== creation_number) continue;
+
+        const token = getTokenAddress(changeData.type);
+        const metadata = await getTokenMetadata(token);
+        if (!metadata) continue withdrawalsLoop;
+        const { decimals, symbol } = metadata;
+        const amount = Number(event.data.amount) / 10 ** decimals;
+
+        receivers[receiver] = {
+          ...receiverData,
+          tokenSent: symbol,
+          amountSent: amount,
+        };
+      }
+    }
+
+    for (const receiver in receivers) {
+      const receiverData = receivers[receiver];
+      const isMissingFields = Object.values(receiverData).some(
+        (value) => !value
       );
-      setPrevTxn(txn);
-    } else {
-      log(`Duplicate of ${txn}`);
+      if (isMissingFields) {
+        delete receivers[receiver];
+        continue;
+      }
+
+      const { tokenReceived, amountReceived, tokenSent, amountSent } =
+        receiverData;
+      const txn = `${version}_${amountReceived}_${tokenReceived}`;
+
+      if (!prevTxns.includes(txn)) {
+        sendAlert(receiverData);
+        log(
+          `${amountReceived} ${tokenReceived} for ${amountSent} ${tokenSent}, ${version}`
+        );
+        setPrevTxn(txn);
+      } else {
+        log(`Duplicate of ${txn}`);
+      }
     }
+  } catch (error) {
+    errorHandler(error);
   }
 }
